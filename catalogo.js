92c02eda30a90ceac0573fc586debd2926fca0eb
katherine vaz de vargas <kathvargas2008@gmail.com>
2026-05-06 14:28:37 -0300

    catalogo.js

--- catalogo.js
+++ catalogo.js
@@ -1,6 +1,4 @@
-const { createElement } = require("react");
-
-const herois=[
+const heroes=[
     {name:'Elesbaunm',type:'guerreiro',level:47},
     {name:'Genovyy',type:'feiticeira',level:35},
     {name:'Marhvix',type:'mago',level:53},
@@ -9,42 +7,43 @@ const herois=[
     {name:'Eustakkiumn',type:'ranger',level:42},
     {name:'Dorotin',type:'bardo',level:29}
 ]
-const heroCard=document.querySelector('#hero-card');
-const grid=document.querySelector('#heroes-grid');
-
- function criarHeroi(heroi){
-    //elemento principal do card
-    const card=document.createElement('div')
-    card.className='hero-card';
-
-    //imagem do heroi
+function criarHeroi(heroi){//recebe os dados do heroi como parametro
+    //cria o elemento principal
+    const card=document.createElement('div');
+    card.classList.add('hero-card');
+    //cria a imagem
     const img=document.createElement('img');
     img.src=`assets/images/${heroi.type}.jpg`;
     img.alt=`imagem do heroi${heroi.name}.jpg`;
 
-    const infoDiv=document.createElement('div')
-    infoDiv.className='hero-info';
-
+    const heroInfo=document.createElement('div');
+    heroInfo.classList.add('hero-info');
+    //cria as info que estao dentro de heroi info
     const spanName=document.createElement('span');
-    spanName.className='hero-name';
+    spanName.classList.add('hero-name');
     spanName.textContent=heroi.name;
 
-    const spanLevel=document.createElement('span')
+    const spanLevel=document.createElement('span');
     spanLevel.textContent=heroi.type;
-
-    infoDiv.appendChild(spanName);
-    infoDiv.appendChild(spanLevel);
+    //adiciona a heroi info, filhos dentro de pai
+    heroInfo.appendChild(spanName);
+    heroInfo.appendChild(spanLevel);
+    //adiciona ao card
     card.appendChild(img);
-    card.appendChild(infoDiv);
+    card.appendChild(heroInfo);
 
     return card;
- }
- function renderizarGrid(){
-    grid.replaceChildren();
-    herois.forEach((heroi)=>{
-    const card=criarHeroi(heroi);
-    grid.appendChild(card);
-    });
- }
- renderizarGrid();
-    
+}
+    function renderizarGrid(){
+        const grid=document.querySelector('#heroes-grid')
+        //limpa o grid
+        grid.replaceChildren();
+        //percorre o array
+        heroes.forEach((heroi)=>{
+        //para cada heroi percorrido no array vai criar o elemento
+        const heroiCard=criarHeroi(heroi);
+        //adiciona ao grid
+        grid.appendChild(heroiCard)
+        })
+    }
+    renderizarGrid();
